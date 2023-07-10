fd4bcf88756fef583240163b039c5887cd49d343
Timur~ <galy.tim@gmail.com>
2023-07-11 01:25:48 +0300

    add delete data from table and db

--- src/App.js
+++ src/App.js
@@ -1,44 +1,66 @@
-import { Button,Table } from 'antd';
+import React, { useState, useEffect, useMemo } from 'react';
+import { Input } from 'antd';
 import DataService from './API/DataService';
-import { useEffect, useState } from 'react';
-import Loader from './components/UI/Loader/Loader';
-import useActGeneration from './hooks/useActGeneration';
+import TableComponent from './components/TableComponent';
 import "./App.css"
 
 function App() {
   const [dataSource, setDataSource] = useState([]);
   const [columns, setColumns] = useState(DataService.getInitColums);
-  const [isDataLoading, setIsDataLoadind] = useState(false);
-  
-  const { generateAct, isActGenerated } = useActGeneration(dataSource, setDataSource, columns, setColumns);
-
-  async function fetchData() {
-    setIsDataLoadind(true);
-    setTimeout(async () => {
-      const response = await DataService.getAll();
-      setDataSource(response.data);
-      setIsDataLoadind(false);
-    }, 1000);
-  }
+  const [isDataLoading, setIsDataLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const fetchData = async () => {
+    setIsDataLoading(true);
+    try {
+      const response = await DataService.getAll();
+      setDataSource(response.data);
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+    } finally {
+      setIsDataLoading(false);
+    }
+  };
+
+  const searchedData = useMemo(() => {
+    return dataSource.filter(eqp => eqp.name.includes(searchQuery));
+  }, [searchQuery, dataSource]);
+
+  const handleDelete = async (index) => {
+    try {
+      const deletedItem = dataSource[index];
+      console.log(deletedItem.id)
+      await DataService.deleteItem(deletedItem.id); 
+      
+      const updatedDataSource = [...dataSource];
+      updatedDataSource.splice(index, 1);
+      setDataSource(updatedDataSource);
+    } catch (error) {
+      console.error('Ошибка при удалении:', error);
+    }
+  };
+  
+
   return (
     <div className='App'>
-      
-      {isDataLoading ? (
-        <div className='load'
-        >
-          <Loader />
-        </div>
-      ) : (
-        <Table dataSource={dataSource} columns={columns} />
-      )}
-      <Button style={{color: "#1677ff"}} onClick={generateAct} disabled={isActGenerated}>
-        Получить акт выбытия
-      </Button>
+      <Input
+        style={{ width: 200 }}
+        placeholder='Поиск'
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+      />
+      <TableComponent
+        dataSource={searchedData}
+        setDataSource={setDataSource}
+        columns={columns}
+        setColumns={setColumns}
+        isDataLoading={isDataLoading}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 }
---
+++ src/components/TableComponent.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Button, Table } from 'antd';
+import useActGeneration from '../hooks/useActGeneration';
+import Loader from '../components/UI/Loader/Loader';
+
+function TableComponent({ dataSource, setDataSource, columns, setColumns, isDataLoading, handleDelete }) {
+  const { generateAct, isActGenerated } = useActGeneration(
+    dataSource,
+    setDataSource,
+    columns,
+    setColumns
+  );
+
+  const columnsWithDelete = [
+    ...columns,
+    {
+      title: 'Действия',
+      key: 'actions',
+      render: (_, record, index) => (
+        <Button type="link" danger onClick={() => handleDelete(index)}>
+          Удалить
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <div className='App'>
+      {isDataLoading ? (
+        <div className='load'>
+          <Loader />
+        </div>
+      ) : (
+        <Table
+          style={{ marginTop: 20 }}
+          dataSource={dataSource}
+          columns={columnsWithDelete}
+        />
+      )}
+      <Button
+        style={{ color: '#1677ff' }}
+        onClick={generateAct}
+        disabled={isActGenerated}
+      >
+        Получить акт выбытия
+      </Button>
+    </div>
+  );
+}
+
+export default TableComponent;
